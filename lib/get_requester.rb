a2fc62305421bc5ad0b7a2b1ca30a1f21bcf117a
Allan Farca <allan.farca@gmail.com>
2020-05-03 08:32:22 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,11 +4,14 @@ require 'json'
 
 class GetRequester
   
+  attr_accessor :url
+  
   def initialize (url)
+    @url = url
   end 
   
   def get_response_body
-    uri = URI.parse(URL)
+    uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
   end 
